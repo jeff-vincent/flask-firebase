d12c5d24f2bc00ec35bfb46e4454ad782d078ab5
jeffreyvincent <jeff.d.vincent@gmail.com>
2018-12-23 16:55:12 -0800

    Parsing requests correctly

--- base_api.py
+++ base_api.py
@@ -1,26 +1,26 @@
 import pyrebase
 
 config = {
-    "apiKey": "AIzaSyCVQZxTHUb1loEoUQdcXBAz4DkyGQOFFB0",
-    "authDomain": "flask-firebase-b6952.firebaseapp.com",
-    "databaseURL": "https://flask-firebase-b6952.firebaseio.com",
-    "projectId": "flask-firebase-b6952",
-    "storageBucket": "flask-firebase-b6952.appspot.com",
-    "messagingSenderId": "218539348402"
+    "apiKey": "",
+    "authDomain": "",
+    "databaseURL": "",
+    "projectId": "",
+    "storageBucket": "",
+    "messagingSenderId": ""
 }
 
 firebase = pyrebase.initialize_app(config)
 
 def sign_up(request):
-    password = request.args.get('password')
-    email = request.args.get('email')
+    password = request.form.get('password')
+    email = request.form.get('email')
     auth = firebase.auth()
     user = auth.create_user_with_email_and_password(email, password)
     return user
 
 def log_in(request):
-    password = request.args.get('password')
-    email = request.args.get('email')
+    password = request.form.get('password')
+    email = request.form.get('email')
     auth = firebase.auth()
     user = auth.sign_in_with_email_and_password(email, password)
     return user
